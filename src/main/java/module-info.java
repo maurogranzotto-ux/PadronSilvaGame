095930264544fedc20ba0f6de640cc7655893df7
maurogranzotto-ux <mauro.granzotto@itiszuccante.edu.it>
2026-05-05 14:04:09 +0200

    Primo commit: struttura JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tdpadronsilva {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tdpadronsilva to javafx.fxml;
+    exports com.example.tdpadronsilva;
+}
